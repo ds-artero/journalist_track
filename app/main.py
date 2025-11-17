174c02125cd6a92015d5c07e02d7b94a418d02a5
ds-artero <d.sanchezartero@gmail.com>
2025-11-17 18:53:56 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
+from fastapi import FastAPI
+from app.routes import search
+
+app = FastAPI(
+    title="Universal Web Search Scraper",
+    description="API that searches websites, finds results, and returns URLs and titles.",
+    version="1.0.0"
+)
+
+app.include_router(search.router)
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to the Web Search Scraper API"}
 
